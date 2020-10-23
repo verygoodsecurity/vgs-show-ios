f94a520aaba36527f2e6a4e4e405274964630eca
dmytro.khl <dmytro.khludkov@vgs.io>
2020-10-23 13:09:27 +0300

    Initial commit.

---
+++ VGSShow/VGSShow/VGSShow.h
@@ -0,0 +1,18 @@
+//
+//  VGSShow.h
+//  VGSShow
+//
+//  Created by Dima on 23.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VGSShow.
+FOUNDATION_EXPORT double VGSShowVersionNumber;
+
+//! Project version string for VGSShow.
+FOUNDATION_EXPORT const unsigned char VGSShowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VGSShow/PublicHeader.h>
+
+
