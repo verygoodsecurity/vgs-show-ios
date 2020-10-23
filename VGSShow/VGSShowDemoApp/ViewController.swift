d31b11c9010ce79a254cf16f5608f2b9c26b23db
dmytro.khl <dmytro.khludkov@vgs.io>
2020-10-23 13:19:08 +0300

    Add demo app.

---
+++ VGSShow/VGSShowDemoApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  VGSShowDemoApp
+//
+//  Created by Dima on 23.10.2020.
+//
+
+import UIKit
+import VGSShow
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
