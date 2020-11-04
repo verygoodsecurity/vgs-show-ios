75167c1ebc1b88fbde275d2767a593348c746924
dmytro.khl <dmytro.khludkov@vgs.io>
2020-11-04 19:38:34 +0200

    Additional label attributes.

---
+++ VGSShow/VGSShowTests/UIElements/VGSLabelTest.swift
@@ -0,0 +1,28 @@
+//
+//  VGSLabelTest.swift
+//  VGSShowTests
+//
+//  Created by Dima on 04.11.2020.
+//
+
+import XCTest
+@testable import VGSShow
+
+class VGSLabelTests: XCTestCase {
+
+  /// Test valid jsonSelectors.
+  func testLabelTextAttributes() {
+    let label = VGSLabel()
+    label.fieldName = "test.label"
+    
+    XCTAssertTrue(label.fieldName == "test.label")
+    XCTAssertTrue(label.isEmpty)
+    
+    label.label.secureText = "123"
+    XCTAssertFalse(label.isEmpty)
+    XCTAssertTrue( label.label.text == nil)
+    
+    label.label.secureText = ""
+    XCTAssertTrue(label.isEmpty)
+  }
+}
