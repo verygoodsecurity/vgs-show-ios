9fe9820de3426410d99689b6e1be5528713bb210
Dima Khludkov <57712402+dmytrokhl@users.noreply.github.com>
2020-11-05 17:14:12 +0200

    Support environment setup with string obj. (#19)

--- VGSShow/VGSShowTests/VGSShowTests.swift
+++ VGSShow/VGSShowTests/VGSShowTests.swift
@@ -9,25 +9,106 @@ import XCTest
 @testable import VGSShow
 
 class VGSShowTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var vgsShow: VGSShow!
+    
+    override func setUp() {
+      vgsShow = VGSShow(id: "test")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+      vgsShow = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testEnvByDefault() {
+        let host = vgsShow.apiClient.baseURL.host ?? ""
+        XCTAssertTrue(host.contains("sandbox"))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+  
+    func testSandboxEnvironmentReturnsTrue() {
+      var liveForm = VGSShow(id: "testID", environment: .sandbox)
+      var host = liveForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.sandbox.verygoodproxy.com")
+    
+      liveForm = VGSShow(id: "testID", environment: .sandbox, dataRegion: "")
+      host = liveForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.sandbox.verygoodproxy.com")
+    
+      liveForm = VGSShow(id: "testID", environment: .sandbox, dataRegion: "ua-0505")
+      host = liveForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.sandbox-ua-0505.verygoodproxy.com")
     }
-
+    
+    func testLiveEnvironmentReturnsTrue() {
+      var liveForm = VGSShow(id: "testID", environment: .live)
+      var host = liveForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.live.verygoodproxy.com")
+    
+      liveForm = VGSShow(id: "testID", environment: .live, dataRegion: "")
+      host = liveForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.live.verygoodproxy.com")
+    
+      liveForm = VGSShow(id: "testID", environment: .live, dataRegion: "ua-0505")
+      host = liveForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.live-ua-0505.verygoodproxy.com")
+    }
+  
+    func testRegionalEnvironmentReturnsTrue() {
+      var liveForm = VGSShow(id: "testID", environment: "live")
+      var host = liveForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.live.verygoodproxy.com")
+    
+      liveForm = VGSShow(id: "testID", environment: "live-eu1")
+      host = liveForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.live-eu1.verygoodproxy.com")
+    
+      liveForm = VGSShow(id: "testID", environment: "live-ua-0505")
+      host = liveForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.live-ua-0505.verygoodproxy.com")
+      
+      
+      var sandboxForm = VGSShow(id: "testID", environment: "sandbox")
+      host = sandboxForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.sandbox.verygoodproxy.com")
+      
+      sandboxForm = VGSShow(id: "testID", environment: "sandbox-ua5")
+      host = sandboxForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.sandbox-ua5.verygoodproxy.com")
+    
+      sandboxForm = VGSShow(id: "testID", environment: "sandbox-ua-0505")
+      host = sandboxForm.apiClient.baseURL.host ?? ""
+      XCTAssertTrue(host == "testID.sandbox-ua-0505.verygoodproxy.com")
+    }
+  
+    func testGenerateRegionalEnvironmentStringReturnsFalse() {
+      let notValidEnvStrings = ["liv", "random-ua1", "random-ua-0505",
+                                "live-", "live.com", "live/eu",
+                                "sandox/us-5050", "sanbox?web=google.com", "", " "]
+      
+      for env in notValidEnvStrings {
+        XCTAssertFalse(VGSShow.regionalEnironmentStringValid(env))
+      }
+    }
+  
+    func testRegionStringValidation() {
+      XCTAssertTrue(VGSShow.regionValid("ua-0505"))
+      XCTAssertTrue(VGSShow.regionValid("ua0505"))
+      
+      XCTAssertFalse(VGSShow.regionValid("ua_0505"))
+      XCTAssertFalse(VGSShow.regionValid("ua:0505"))
+      XCTAssertFalse(VGSShow.regionValid("ua-0505/verygoodsecurity.com"))
+      XCTAssertFalse(VGSShow.regionValid("ua-0505?param=val"))
+      XCTAssertFalse(VGSShow.regionValid("ua-0505#val,id=ua-0505&env=1"))
+    }
+    
+    func testCustomHeader() {
+        let headerKey = "costom-header"
+        let headerValue = "custom header value"
+        
+        vgsShow.customHeaders = [
+            headerKey: headerValue
+        ]
+        
+        XCTAssertNotNil(vgsShow.customHeaders)
+        XCTAssert(vgsShow.customHeaders![headerKey] == headerValue)
+    }
 }
