e85aaa0af50e16afe071239e0ea89d95c1a6f369
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2020-11-12 11:51:57 +0200

    Setup project

--- Pods/Target Support Files/Pods-VGSShowDemoApp/Pods-VGSShowDemoApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VGSShowDemoApp : NSObject
-@end
-@implementation PodsDummy_Pods_VGSShowDemoApp
-@end
--- Pods/Target Support Files/VGSCollectSDK/VGSCollectSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_VGSCollectSDK : NSObject
-@end
-@implementation PodsDummy_VGSCollectSDK
-@end
