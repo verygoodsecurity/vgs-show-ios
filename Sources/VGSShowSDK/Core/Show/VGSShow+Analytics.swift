ec72f86c7b598c85d9705b2900c9785741034208
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-05-24 08:23:45 +0300

    Track view type

--- Sources/VGSShowSDK/Core/Show/VGSShow+Analytics.swift
+++ Sources/VGSShowSDK/Core/Show/VGSShow+Analytics.swift
@@ -9,14 +9,14 @@ internal extension VGSShow {
 	/// Track subscribe event for view.
 	/// - Parameter view: `VGSViewProtocol` view to track.
 	func trackSubscribeEvent(for view: VGSViewProtocol) {
-		let extraData: [String: Any] = ["contentPath": view.contentPath as Any]
+		let extraData = extraData(for: view)
 		VGSAnalyticsClient.shared.trackFormEvent(self, type: .fieldInit, extraData: extraData)
 	}
 
 	/// Track unsubscribe event for view.
 	/// - Parameter view: `VGSViewProtocol` view to track.
 	func trackUnsubscribeEvent(for view: VGSViewProtocol) {
-		let extraData: [String: Any] = ["contentPath": view.contentPath as Any]
+		let extraData = extraData(for: view)
 		VGSAnalyticsClient.shared.trackFormEvent(self, type: .fieldUnsubscibe, extraData: extraData)
 	}
   
@@ -37,4 +37,34 @@ internal extension VGSShow {
     }
   }
 
+	/// View analytics extra data.
+	/// - Parameter view: `VGSViewProtocol` object.
+	/// - Returns: `[String: Any]` object, view extra data analytics.
+	func extraData(for view: VGSViewProtocol) -> [String: Any] {
+		var extraData: [String: Any] = ["contentPath": view.contentPath as Any]
+		if let viewTypeName = viewTypeName(for: view) {
+			extraData["field"] = viewTypeName
+		}
+
+		return extraData
+	}
+
+	/// View type name for analytics.
+	/// - Parameter view: `VGSViewProtocol` object, view to get type name.
+	/// - Returns: `String?`, view type name.
+	func viewTypeName(for view: VGSViewProtocol) -> String? {
+		if #available(iOS 11.0, *) {
+			if view is VGSPDFView {
+				return "pdf"
+			}
+		} else {
+			// Fallback on earlier versions
+		}
+
+		if view is VGSLabel {
+			return "text"
+		}
+
+		return nil
+	}
 }
