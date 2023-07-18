5bebea837b35002ce0050d5724cf8472fdf57950
Donald Rodriguez <donald.rodriguez@verygoodsecurity.com>
2023-07-17 18:25:58 -0600

    Update accessibility properties

--- Sources/VGSShowSDK/UIElements/VGSLabel/VGSLabel.swift
+++ Sources/VGSShowSDK/UIElements/VGSLabel/VGSLabel.swift
@@ -231,6 +231,17 @@ public final class VGSLabel: UIView, VGSLabelProtocol {
             placeholderLabel.accessibilityLabel = label.accessibilityLabel
         }
     }
+    
+    /// A succinct label in a localized string that
+    /// identifies the accessibility element.
+    public override var accessibilityLabel: String? {
+        get {
+            return vgsAccessibilityLabel
+        }
+        set {
+            vgsAccessibilityLabel = newValue
+        }
+    }
 
     /// A localized string that contains a brief description of the result of
     /// performing an action on the accessibility element.
@@ -244,6 +255,17 @@ public final class VGSLabel: UIView, VGSLabelProtocol {
         }
     }
     
+    /// A localized string that contains a brief description of the result of
+    /// performing an action on the accessibility element.
+    public override var accessibilityHint: String? {
+        get {
+            return vgsAccessibilityHint
+        }
+        set {
+            vgsAccessibilityHint = newValue
+        }
+    }
+    
     /// Boolean value that determinates if the text field should be exposed as
     /// an accesibility element.
     public var vgsIsAccessibilityElement: Bool {
@@ -253,6 +275,28 @@ public final class VGSLabel: UIView, VGSLabelProtocol {
         set {
             label.isAccessibilityElement = newValue
             placeholderLabel.isAccessibilityElement = label.isAccessibilityElement
+        }
+    }
+    
+    /// Localized string that represents the value of the element
+    public override var accessibilityValue: String? {
+        get {
+            return nil
+        }
+        set {
+            label.accessibilityValue = newValue
+            placeholderLabel.accessibilityValue = label.accessibilityValue
+        }
+    }
+    
+    /// Boolean value that determinates if the text field should be exposed as
+    /// an accesibility element.
+    public override var isAccessibilityElement: Bool {
+        get {
+            return vgsIsAccessibilityElement
+        }
+        set {
+            vgsIsAccessibilityElement = newValue
         }
     }
     
--- Tests/VGSShowSDKTests/UIElements/VGSLabelTest.swift
+++ Tests/VGSShowSDKTests/UIElements/VGSLabelTest.swift
@@ -9,20 +9,57 @@ import XCTest
 @testable import VGSShowSDK
 
 class VGSLabelTests: XCTestCase {
-
-  /// Test valid jsonSelectors.
-  func testLabelTextAttributes() {
-    let label = VGSLabel()
-    label.contentPath = "test.label"
     
-    XCTAssertTrue(label.contentPath == "test.label")
-    XCTAssertTrue(label.isEmpty)
+    /// Test valid jsonSelectors.
+    func testLabelTextAttributes() {
+        let label = VGSLabel()
+        label.contentPath = "test.label"
+        
+        XCTAssertTrue(label.contentPath == "test.label")
+        XCTAssertTrue(label.isEmpty)
+        
+        label.label.secureText = "123"
+        XCTAssertFalse(label.isEmpty)
+        XCTAssertTrue(label.label.text == nil)
+        
+        label.label.secureText = ""
+        XCTAssertTrue(label.isEmpty)
+    }
     
-    label.label.secureText = "123"
-    XCTAssertFalse(label.isEmpty)
-    XCTAssertTrue( label.label.text == nil)
-    
-    label.label.secureText = ""
-    XCTAssertTrue(label.isEmpty)
-  }
+    /// Test accessibility properties
+    func testLabelAccessibilityAttributes() {
+        let labelView = VGSLabel()
+        
+        // Hint
+        let accHint = "accessibility hint"
+        labelView.vgsAccessibilityHint = accHint
+        XCTAssertNotNil(labelView.vgsAccessibilityHint)
+        XCTAssertEqual(labelView.vgsAccessibilityHint, accHint)
+        
+        labelView.accessibilityHint = accHint
+        XCTAssertNotNil(labelView.accessibilityHint)
+        XCTAssertEqual(labelView.accessibilityHint, accHint)
+        
+        // Label
+        let accLabel = "accessibility label"
+        labelView.vgsAccessibilityLabel = accLabel
+        XCTAssertNotNil(labelView.vgsAccessibilityLabel)
+        XCTAssertEqual(labelView.vgsAccessibilityLabel, accLabel)
+        
+        labelView.accessibilityLabel = accLabel
+        XCTAssertNotNil(labelView.accessibilityLabel)
+        XCTAssertEqual(labelView.accessibilityLabel, accLabel)
+        
+        // Element
+        labelView.vgsIsAccessibilityElement = true
+        XCTAssertTrue(labelView.vgsIsAccessibilityElement)
+        
+        labelView.isAccessibilityElement = true
+        XCTAssertTrue(labelView.isAccessibilityElement)
+        
+        // Value
+        let accValue = "acc value"
+        labelView.accessibilityValue = accValue
+        XCTAssertNil(labelView.accessibilityValue)
+    }
 }
