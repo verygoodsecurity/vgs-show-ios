130e2ad49f0972251797cb9ab70ea8edf997ff87
Dmytro Khludkov <dmytro.khludkov@vgs.io>
2023-11-20 17:00:23 +0200

    PDFView tests.

---
+++ Tests/VGSShowSDKTests/UIElements/VGSShowPdfViewModelTests.swift
@@ -0,0 +1,49 @@
+//
+//  VGSShowPdfViewModelTests.swift
+//  VGSShowSDKTests
+//
+
+import XCTest
+@testable import VGSShowSDK
+
+class VGSShowPdfViewModelTests: XCTestCase {
+
+    var viewModel: VGSShowPdfViewModel!
+
+    override func setUp() {
+        super.setUp()
+        viewModel = VGSShowPdfViewModel()
+    }
+
+    func testHandleDecodingResultSuccess() {
+        let expectedData = Data(repeating: 0, count: 5)
+        let decodingResult: VGSShowDecodingResult = .success(.rawData(expectedData))
+
+        let expectation = self.expectation(description: "Value changed")
+        viewModel.onValueChanged = { value in
+            if case let .rawData(data)? = value {
+                XCTAssertEqual(data, expectedData, "Value should be updated with expected raw data")
+            } else {
+                XCTFail("Value should be of type VGSShowPDFContent.rawData")
+            }
+            expectation.fulfill()
+        }
+
+        viewModel.handleDecodingResult(decodingResult)
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
+    func testHandleDecodingResultFailure() {
+        let expectedError = VGSShowError(type: .invalidBase64Data)
+        let decodingResult: VGSShowDecodingResult = .failure(expectedError)
+
+        let expectation = self.expectation(description: "Error occurred")
+        viewModel.onError = { error in
+            XCTAssertEqual(error.type, expectedError.type, "Error type should be the expected type")
+            expectation.fulfill()
+        }
+
+        viewModel.handleDecodingResult(decodingResult)
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+}
